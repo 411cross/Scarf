a75c165a846aba73b47d892bfc02b1a4ea423321
zhengbuxuan <zhengbuxuan@gmail.com>
2017-11-21 10:07:40 +0800

    ADD StatusList to ParticularBean ADD APIs

---
+++ app/src/main/java/com/example/jungle/weixin/Bean/ParticularBean/StatusList.java
@@ -0,0 +1,13 @@
+package com.example.jungle.weixin.Bean.ParticularBean;
+
+import com.example.jungle.weixin.Bean.BaseBean.Status;
+
+/**
+ * Created by derrickJ on 2017/11/21.
+ */
+
+public class StatusList {
+
+    private Status[] statuses;
+
+}
