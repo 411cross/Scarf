35498b1dc677546e731a10c13a901ab93f72840a
jungle <976207331@qq.com>
2017-11-24 16:05:45 +0800

    补充丢失的fan and follow

--- app/src/main/java/com/example/jungle/weixin/Activity/FollowActivity.java
+++ app/src/main/java/com/example/jungle/weixin/Activity/FollowActivity.java
@@ -96,4 +96,5 @@ public class FollowActivity extends AppCompatSwipeBack {
     }
 
 
+
 }
--- app/src/main/java/com/example/jungle/weixin/Adapter/FollowFansAdapter.java
+++ app/src/main/java/com/example/jungle/weixin/Adapter/FollowFansAdapter.java
@@ -68,4 +68,5 @@ public class FollowFansAdapter extends RecyclerView.Adapter<FollowFansAdapter.Vi
 
     }
 
+
 }
