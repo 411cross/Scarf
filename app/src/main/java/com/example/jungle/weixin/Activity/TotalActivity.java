0cc9c35eb701063993732f313cab519153138827
chf <1174971877@qq.com>
2017-12-04 11:26:07 +0800

    版本号改成1.1 去掉restart

--- app/src/main/java/com/example/jungle/weixin/Activity/TotalActivity.java
+++ app/src/main/java/com/example/jungle/weixin/Activity/TotalActivity.java
@@ -277,10 +277,6 @@ public class TotalActivity extends AppCompatActivity implements View.OnClickList
 
         android.os.Process.killProcess(android.os.Process.myPid());    //获取PID
         System.exit(0);   //常规java、c#的标准退出法，返回值为0代表正常退出
-        ActivityManager am = (ActivityManager)getSystemService (Context.ACTIVITY_SERVICE);
-        am.restartPackage(getPackageName());
 }
-
-
 
 }
